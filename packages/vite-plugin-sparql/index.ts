65ec441c5f26656669b125d01134c9f10d4a24fc
tpluscode <tpluscode@users.noreply.github.com>
2026-02-19 13:05:45 +0100

    refactor: plugin system with build support

--- packages/vite-plugin-sparql/index.ts
+++ packages/vite-plugin-sparql/index.ts
@@ -1,37 +1,16 @@
-import { Plugin } from 'vite';
-import { load } from 'cheerio'
-import { sparqlToMetaScript } from './sparqlToMetaScript.js';
+import {Plugin} from 'vite';
 import {compile} from "sparqlc";
-import * as fs from "node:fs";
 
-export function selectMeta(endpoint: string): Plugin {
-    return {
-        name: 'vite-plugin-sparql-select-meta',
-        transformIndexHtml: {
-            order: 'pre',
-            handler(html, ctx) {
-                //const dom = parse(html)
-                const $ = load(html)
+export default <Plugin>{
+    name: 'vite-plugin-sparql',
+    transform(code, id) {
+        if (id.endsWith('.rq')) {
+            const compiled = compile(code)
+            compiled.writeTypes(id)
 
-                const metaScriptSrc = ctx.filename + '.meta.rq'
-                if (fs.existsSync(metaScriptSrc)) {
-                    const metaScript = sparqlToMetaScript(fs.readFileSync(metaScriptSrc).toString(), endpoint)
-
-                    $('head').append(`<script type="module" ssr>${metaScript}</script>`)
-                }
-
-                return $.html()
-            }
-        },
-        transform(code, id) {
-            if (id.endsWith('.rq')) {
-                const compiled = compile(code)
-                compiled.writeTypes(id)
-
-                return 'export default ' + compiled.code
-            }
-
-            return code
+            return 'export default ' + compiled.code
         }
+
+        return code
     }
 }
