3a2666d8ad9a0017c6b747951dc59045b5ca3a39
tpluscode <tpluscode@users.noreply.github.com>
2026-02-19 14:21:21 +0100

    feat: build server pages modules with esbuild

---
+++ packages/esbuild-plugin-sparql/index.ts
@@ -0,0 +1,21 @@
+import type { Plugin } from 'esbuild';
+import { readFile } from 'node:fs/promises';
+import { compile } from 'sparqlc';
+
+const sparqlPlugin: Plugin = {
+  name: 'sparql-loader',
+  setup(build) {
+    build.onLoad({ filter: /\.rq$/ }, async (args) => {
+      const contents = await readFile(args.path, 'utf8');
+      const compiled = compile(contents);
+      compiled.writeTypes(args.path);
+
+      return {
+        contents: `export default ${compiled.code}`,
+        loader: 'js',
+      };
+    });
+  },
+};
+
+export default sparqlPlugin;
