f913d958c24112ddcce071e1076745e82fb7bc91
tpluscode <tpluscode@users.noreply.github.com>
2026-02-24 18:50:33 +0100

    fix; missing file

---
+++ packages/sparqlc/moduleTemplate.js
@@ -0,0 +1,36 @@
+import TermMap from '@rdfjs/term-map'
+import { isEnv, toTermMap } from 'sparqlc/runtime.js'
+import Processor from 'sparqlc/processor.js'
+
+export default async function (...args) {
+  const { client, env, processors = [] } = args.pop()
+
+  if (!isEnv(env)) {
+    throw new Error('Parameters must be followed by executor options. `env` is required.')
+  }
+
+  const params = args.reduce((map, p) => toTermMap(map, p), new TermMap())
+  const query = queryObject
+
+  const paramsProcessor = new Processor(env, params)
+
+  const { default: sparqljs } = await import('sparqljs')
+  const { Generator } = sparqljs
+
+  const processed = [paramsProcessor, ...processors].reduce((query, processor) => processor.process(query), query)
+  if (query.type !== 'query') {
+    throw new Error('Only queries are supported')
+  }
+
+  let method = query.queryType.toLowerCase()
+  if (method === 'describe') {
+    method = 'construct'
+  }
+
+  const queryString = new Generator().stringify(processed)
+  if (!client) {
+    return queryString
+  }
+
+  return client.query[method](queryString)
+}
